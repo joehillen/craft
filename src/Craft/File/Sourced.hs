c10dc2df1d55591ed1c1fbea83fb91d73762568e
Joe Hillenbrand <joehillen@gmail.com>
2016-01-12 17:42:24 -0800

    make file an element of SourcedFile

--- src/Craft/File/Sourced.hs
+++ src/Craft/File/Sourced.hs
@@ -10,15 +10,32 @@ import qualified Craft.Group as Group
 import           Craft.Internal.FileDirectory
 import qualified Craft.File as File
 
+import Control.Lens
 import           System.FilePath
 
 
 data SourcedFile
   = SourcedFile
-    { path    :: FilePath
-    , mode    :: Mode
-    , ownerID :: UserID
-    , groupID :: GroupID
-    , source  :: FilePath
+    { _file    :: File.File
+    , _source  :: FilePath
     }
   deriving (Eq, Show)
+
+makeLenses ''SourcedFile
+
+
+sourcedFile :: FilePath -> FilePath -> SourcedFile
+sourcedFile fp source' =
+  SourcedFile
+  { _file = File.file fp
+  , _source = source'
+  }
+
+
+instance Craftable SourcedFile where
+  craft_ sf = do
+    let sf' = sf & file . File.content .~ Nothing
+    sourceFile (sf' ^. source) (sf' ^. file . File.path)
+    craft_ $ sf' ^. file
+
+  watchCraft sf = $notImplemented $ "watchCraft " ++ show sf
