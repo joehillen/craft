f75276aa0eba60103b17e7c87a8111ce1bb00dc3
Joe Hillenbrand <joehillen@gmail.com>
2016-01-19 16:12:18 -0800

    Add Authorization support to S3File
    ... and verify after downloading.

--- src/Craft/S3File.hs
+++ src/Craft/S3File.hs
@@ -1,12 +1,21 @@
 module Craft.S3File where
 
+-- bytestring
+import qualified Data.ByteString.Char8 as B8
+-- lens
+import Control.Lens hiding (noneOf)
+-- cryptonite
+import Crypto.Hash (SHA1)
+import Crypto.MAC.HMAC (HMAC, hmac, hmacGetDigest)
+-- memory
+import Data.ByteArray.Encoding
+-- megaparsec
+import Text.Megaparsec
+import Text.Megaparsec.String
+
 import           Craft
 import           Craft.File (File)
 import qualified Craft.File as File
-
-import Control.Lens hiding (noneOf)
-import Text.Megaparsec
-import Text.Megaparsec.String
 
 
 data S3File
@@ -15,8 +24,10 @@ data S3File
     , _source  :: String
     , _domain  :: String
     , _version :: Version
+    , _auth    :: Maybe (String, String) -- ^ (AWSAccessKeyId, AWSSecretAccessKey)
     }
     deriving (Eq, Show)
+
 makeLenses ''S3File
 
 
@@ -27,6 +38,7 @@ s3file fp source' =
   , _domain  = "s3.amazonaws.com"
   , _source  = source'
   , _version = AnyVersion
+  , _auth    = Nothing
   }
 
 
@@ -34,17 +46,53 @@ url :: Getter S3File String
 url = to (\f -> "https://" ++ f ^. domain ++ "/" ++ f ^. source)
 
 
+-- | Add AWS Authentication Headers to curl commands
+-- http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html#ConstructingTheAuthenticationHeader
+authHeaders :: String -> S3File -> Craft [String]
+authHeaders method s3f =
+  case s3f ^. auth of
+    Nothing -> return []
+    Just (awsKeyID, awsSecretKey) -> do
+      date <- view stdout <$> ($errorOnFail =<< exec "date" ["-u", "--rfc-2822"])
+      url <- case s3f ^. source of
+               []    -> $craftError $ "S3File.source is empty! " ++ show s3f
+               '/':_ -> return $ s3f ^. source
+               xs    -> return $ '/':(s3f ^. source)
+      let contentMD5 = ""
+          contentType = ""
+          canonicalizedAmzHeaders = ""
+          toSign = method ++ "\n"
+                ++ contentMD5 ++ "\n"
+                ++ contentType ++ "\n"
+                ++ date ++ "\n"
+                ++ canonicalizedAmzHeaders
+                ++ url
+          sigHMAC :: HMAC SHA1
+          sigHMAC = hmac (B8.pack awsSecretKey)
+                         (B8.pack toSign)
+          sig = B8.unpack $ convertToBase Base64 sigHMAC
+      $logDebug $ "awsSecretKey == " ++ awsSecretKey
+      $logDebug $ "awsKeyID == " ++ awsKeyID
+      $logDebug $ "toSign == " ++ show toSign
+
+      return [ "--header", "Date:" ++ date
+             , "--header", "Authorization:AWS " ++ awsKeyID ++ ":" ++ sig
+             ]
+
+-- auth_header = "Authorization:AWS "+AWSAccessKeyId+":"+sig
+-- date_header = "Date:"+ amazons3time
 
 getS3Sum :: S3File -> Craft (Maybe String)
 getS3Sum f = do
-  r <- $errorOnFail =<< exec "curl" ["-I", f ^. url]
+  hdrs <- authHeaders "HEAD" f
+  r <- $errorOnFail =<< exec "curl" (hdrs ++ ["-XHEAD", "-I", f ^. url])
   headers <- parseExecResult (ExecSucc r) httpHeaders $ r ^. stdout
   return $ filter ('"' /=) <$> lookup "ETag" headers
 
 
 httpHeaders :: Parser [(String, String)]
 httpHeaders = do
-  _ <- string "HTTP/" >> digitChar >> char '.' >> digitChar >> string " 200 OK" >> eol
+  _ <- string "HTTP/1." >> oneOf "01" >> string " 200 OK" >> eol
   some header
 
 
@@ -58,8 +106,15 @@ header = do
 instance Craftable S3File where
   watchCraft s3f = do
     let s3f' = s3f & file . File.content .~ Nothing
-    let fp = s3f' ^. file . File.path
-    let downloadFile = exec_ "curl" ["-s", "-L", "-o", fp, s3f' ^. url]
+        fp = s3f' ^. file . File.path
+        downloadFile = do
+          hdrs <- authHeaders "GET" s3f
+          exec_ "curl" $ hdrs ++ ["-XGET", "-s", "-L", "-o", fp, s3f' ^. url]
+        verify expected = do
+          sum <- File.md5sum fp
+          when (sum /= expected) (
+            $craftError $ "verify S3File failed! Excepted `" ++ expected ++ "` "
+                       ++ "Got `" ++ sum ++ "` for " ++ show s3f')
 
     getS3Sum s3f' >>= \case
       Nothing -> $craftError $ "Failed to get chksum from S3 for: " ++ show s3f'
@@ -68,19 +123,28 @@ instance Craftable S3File where
         w <- if exists then do
                 curSum <- File.md5sum fp
                 case s3f' ^. version of
-                  AnyVersion     -> return Unchanged
-                  Latest         -> if etag == curSum then
-                                      return Unchanged
-                                    else do
-                                      downloadFile
-                                      return Updated
-                  Version verStr -> if curSum == verStr then
-                                      return Unchanged
-                                    else do
-                                      downloadFile
-                                      return Updated
+                  AnyVersion -> return Unchanged
+                  Latest
+                    | etag == curSum -> return Unchanged
+                    | otherwise -> do
+                        downloadFile
+                        verify etag
+                        return Updated
+                  Version verStr
+                    | curSum == verStr -> return Unchanged
+                    | verStr /= etag ->
+                        $craftError $
+                           "Cannot download specific file version from S3. "
+                           ++ "Found version " ++ etag ++ " for " ++ show s3f'
+                    | otherwise -> do
+                        downloadFile
+                        verify verStr
+                        return Updated
               else do
                 downloadFile
+                case s3f' ^. version of
+                  Version verStr -> verify verStr
+                  _              -> verify etag
                 return Created
 
         fw <- watchCraft_ $ s3f' ^. file
