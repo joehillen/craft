3e8a2a3907872e5357bc29ba89650af5b81db4a1
Joe Hillenbrand <joehillen@gmail.com>
2016-05-11 23:22:13 -0700

    cleanup SSH.PrivateKey

--- src/Craft/SSH/PrivateKey.hs
+++ src/Craft/SSH/PrivateKey.hs
@@ -1,32 +1,35 @@
 module Craft.SSH.PrivateKey where
 
+import           Control.Lens
+
 import           Craft
-import           Craft.SSH
-import qualified Craft.Directory as Directory
+import qualified Craft.Directory as Dir
 import           Craft.File (file)
 import qualified Craft.File as File
 import           Craft.File.Mode
+import           Craft.SSH
 import           Craft.User (User)
 import qualified Craft.User as User
 
-import Control.Lens
 
 data PrivateKey
   = PrivateKey
-    { _user :: User
-    , _name :: String
+    { _user    :: User
+    , _name    :: String
     , _content :: String
     }
   deriving (Eq, Show)
 makeLenses ''PrivateKey
 
-path :: PrivateKey -> FilePath
-path pk = userDir (pk ^. user) ^. Directory.path </> (pk ^. name)
+
+path :: Getter PrivateKey FilePath
+path = to $ \pk -> userDir (pk ^. user) ^. Dir.path </> (pk ^. name)
+
 
 instance Craftable PrivateKey PrivateKey where
   watchCraft pk = do
     craft_ $ userDir $ pk ^. user
-    w <- watchCraft_ $ file (path pk)
+    w <- watchCraft_ $ file (pk ^. path)
                        & File.mode          .~ Mode RW O O
                        & File.ownerAndGroup .~ pk ^. user
                        & File.strContent    .~ pk ^. content
