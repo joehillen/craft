d93a98e6530c611ab19ad89c6421047f9d72d332
Joe Hillenbrand <joehillen@gmail.com>
2016-08-16 22:32:23 -0700

    rename SourcedFile.file to SourcedFile.destination

--- src/Craft/File/Sourced.hs
+++ src/Craft/File/Sourced.hs
@@ -8,54 +8,60 @@ import qualified Craft.File as File
 
 data SourcedFile
   = SourcedFile
-    { _file    :: File
-    , _source  :: FilePath
+    { _destination :: File
+    , _source      :: FilePath
     }
   deriving (Eq, Show)
 
 makeLenses ''SourcedFile
 
--- TODO: instance FileLike SourcedFile
 
 sourcedFile :: FilePath -> FilePath -> SourcedFile
 sourcedFile fp source' =
   SourcedFile
-  { _file = Craft.file fp
+  { _destination = Craft.file fp
   , _source = source'
   }
 
 
+instance FileLike SourcedFile where
+  type FileLikePath SourcedFile = FilePath
+  path    = destination . filePath
+  mode    = destination . fileMode
+  ownerID = destination . fileOwnerID
+  groupID = destination . fileGroupID
+
 
 instance Craftable SourcedFile SourcedFile where
   craft sf = do
-    let sf' = sf & Craft.File.Sourced.file . fileContent .~ Nothing
-    sourceFile (sf' ^. source) (sf' ^. Craft.File.Sourced.file . filePath)
-    craft_ $ sf' ^. Craft.File.Sourced.file
+    let sf' = sf & destination . fileContent .~ Nothing
+    sourceFile (sf'^.source) (sf'^.path)
+    craft_ $ sf' ^. destination
     return sf'
 
   watchCraft sf = do
-    let fp = sf ^. Craft.File.Sourced.file . filePath
-    let sf' = sf & Craft.File.Sourced.file . fileContent .~ Nothing
+    let fp = sf ^. destination . filePath
+    let sf' = sf & destination . fileContent .~ Nothing
     exists <- File.exists fp
     if exists then do
       oldsum <- File.md5sum fp
-      sourceFile (sf ^. source) fp
-      fw <- watchCraft_ $ sf' ^. Craft.File.Sourced.file
+      sourceFile (sf^.source) fp
+      fw <- watchCraft_ $ sf' ^. destination
       newsum <- File.md5sum fp
       if oldsum /= newsum then
         return (Updated, sf')
       else
         return (fw, sf')
     else do
-      sourceFile (sf ^. source) fp
-      craft_ $ sf' ^. Craft.File.Sourced.file
+      sourceFile (sf^.source) fp
+      craft_ $ sf' ^. destination
       return (Created, sf')
 
 
 instance Destroyable SourcedFile where
   watchDestroy sf = do
-    (w, mbf) <- watchDestroy $ sf^.Craft.File.Sourced.file
+    (w, mbf) <- watchDestroy $ sf^.destination
     let res = case mbf of
                 Nothing -> Nothing
-                Just f  -> Just $ sf & Craft.File.Sourced.file .~ f
+                Just f  -> Just $ sf & destination .~ f
     return (w, res)
